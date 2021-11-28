cc4751e0663e3d564698fb574618eed6b432f1dc
Kevin Hansberger <kev@thehansbergers.com>
2021-11-27 23:43:24 -0600

    commit

--- src/screens/Volunteer.js
+++ src/screens/Volunteer.js
@@ -6,14 +6,7 @@ const Volunteer = () => {
       <div class="body-spacing">
           <div class="top-bar"><h1 class="top-bar-text">Politician</h1></div>
           <h2 class="page-title">Volunteer</h2>
-          <div class="nav-img-text-container">
-          <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
-          <div class="nav-img-text-centered"><a class="nav-img-text-style" href="/donate">The Flight Plan</a></div>
-          </div>
-          <div class="nav-img-text-container">
-          <a href="https://jrg.gop/donate" target="_blank"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
-          <div class="nav-img-text-centered"><a class="nav-img-text-style" href="https://jrg.gop/donate" target="_blank">Donate Online</a></div>
-          </div>
+          <iframe src="https://msgsndr.com/widget/form/4cOTWF71kr12F3ScTa16" scrolling="no" class="ghl-form" sandbox=''/>
             <BottomNavBar name='volunteer' />
         </div>
     )
