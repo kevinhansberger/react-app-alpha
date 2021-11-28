30885b68814b0ebec52c03576294b2ae1a1b6aec
Kevin Hansberger <kev@thehansbergers.com>
2021-11-27 20:19:00 -0600

    commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,11 +7,11 @@ const Home = () => {
             <h1>home</h1>
             <div class="nav-img-text-container">
             <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
-            <div class="nav-img-text-centered"><a href="/donate">The Flight Plan</a></div>
+            <div class="nav-img-text-centered"><a style="text-decoration: none; text-decoration-color: white;" href="/donate">The Flight Plan</a></div>
             </div>
             <div class="nav-img-text-container">
             <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
-            <div class="nav-img-text-centered"><a href="/donate">Upcoming Events</a></div>
+            <div class="nav-img-text-centered"><a style="text-decoration: none; text-decoration-color: white;" href="/donate">Upcoming Events</a></div>
             </div>
             <BottomNavBar name='home' />
         </div>
