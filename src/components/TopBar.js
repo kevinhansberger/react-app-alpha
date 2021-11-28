d868990bb9427a5892d8cde32a9e38943a600a16
Kevin Hansberger <kev@thehansbergers.com>
2021-11-27 21:41:23 -0600

    commit

---
+++ src/components/TopBar.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+const TopBar = props => {
+
+    return (
+        <div className='bottom-nav'>
+            <div className='top-tab'>
+            <h1>Politician</h1>
+            </div>
+        </div>
+    )
+}
+
+export default TopBar
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import BottomNavBar from '../components/BottomNavBar'
+import TopBar from '../components/TopBar'
 
 const Home = () => {
     return (
         <div>
+            <TopBar />
             <h1>home</h1>
             <div class="nav-img-text-container">
             <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
@@ -13,6 +15,7 @@ const Home = () => {
             <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
             <div class="nav-img-text-centered"><a class="nav-img-text-style" href="/donate">Upcoming Events</a></div>
             </div>
+            <br /><br />
             <BottomNavBar name='home' />
         </div>
     )
