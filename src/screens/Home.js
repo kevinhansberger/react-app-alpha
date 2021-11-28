e695f7b441ec6f76b15fad82a93efb727e02623a
Kevin Hansberger <kev@thehansbergers.com>
2021-11-27 22:11:46 -0600

    commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import BottomNavBar from '../components/BottomNavBar'
-import TopBar from '../components/TopBar'
 
 const Home = () => {
     return (
         <div>
-            <TopBar name='home' />
-            <h1>home</h1>
+            <div class="top-bar"><h1>home</h1></div>
             <div class="nav-img-text-container">
             <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
             <div class="nav-img-text-centered"><a class="nav-img-text-style" href="/donate">The Flight Plan</a></div>
