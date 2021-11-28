60ad95af78d9cf759f3570c9026da5dab93b405a
Kevin Hansberger <kev@thehansbergers.com>
2021-11-27 19:56:18 -0600

    Update Donate.js

--- src/screens/Donate.js
+++ src/screens/Donate.js
@@ -3,10 +3,14 @@ import BottomNavBar from '../components/BottomNavBar'
 
 const Donate = () => {
     return (
-        <div>
-            <h1>donate</h1>
-            <BottomNavBar name='donate' />
-        </div>
+      <div>
+          <h1>donate</h1>
+          <div class="nav-img-text-container">
+          <a href="https://jrg.gop/donate" target="_blank"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
+          <div class="nav-img-text-centered"><a href="https://jrg.gop/donate" target="_blank">Donate Online</a></div>
+          </div>
+          <BottomNavBar name='home' />
+      </div>
     )
 }
 
