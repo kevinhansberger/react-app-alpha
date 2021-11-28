8b4cd72e4c9a3240192f9fdc4b7208528e5389d0
Kevin Hansberger <kev@thehansbergers.com>
2021-11-27 20:26:01 -0600

    commit

--- src/screens/Donate.js
+++ src/screens/Donate.js
@@ -7,7 +7,7 @@ const Donate = () => {
           <h1>donate</h1>
           <div class="nav-img-text-container">
           <a href="https://jrg.gop/donate" target="_blank"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
-          <div class="nav-img-text-centered"><a style="text-decoration: none; text-decoration-color: white;" href="https://jrg.gop/donate" target="_blank">Donate Online</a></div>
+          <div class="nav-img-text-centered"><a class="nav-img-text-style" href="https://jrg.gop/donate" target="_blank">Donate Online</a></div>
           </div>
           <BottomNavBar name='donate' />
       </div>
