1c523d73d3cb37c77e3c8d03ab75d310b4359856
Kevin Hansberger <kev@thehansbergers.com>
2021-11-28 17:51:41 -0600

    commit

--- src/screens/Volunteer.js
+++ src/screens/Volunteer.js
@@ -6,7 +6,7 @@ const Volunteer = () => {
       <div class="body-spacing">
           <div class="top-bar"><h1 class="top-bar-text">Politician</h1></div>
           <h2 class="page-title">Volunteer</h2>
-          <iframe src="https://msgsndr.com/widget/form/4cOTWF71kr12F3ScTa16" scrolling="no" id="4cOTWF71kr12F3ScTa16" class="ghl-form" sandbox=''/>
+          <iframe src="https://msgsndr.com/widget/form/4cOTWF71kr12F3ScTa16" scrolling="no" id="4cOTWF71kr12F3ScTa16" class="ghl-form" sandbox='allow-scripts allow-same-origin'/>
             <BottomNavBar name='volunteer' />
         </div>
     )
