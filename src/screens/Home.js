ed2118ab7f42c19aff31f77c315edb72126c467e
Kevin Hansberger <kev@thehansbergers.com>
2021-11-27 19:16:57 -0600

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
             <div class="nav-img-text-container">
             <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
             <div class="nav-img-text-centered">The Flight Plan</div>
+            </div>
+            <div class="nav-img-text-container">
             <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
             <div class="nav-img-text-centered">Upcoming Events</div>
             </div>
