32c660b223e698a0f80f7cb5f824cd2fd7871fa7
Kevin Hansberger <kev@thehansbergers.com>
2021-11-27 20:39:29 -0600

    Update Volunteer.js

--- src/screens/Volunteer.js
+++ src/screens/Volunteer.js
@@ -5,6 +5,22 @@ const Volunteer = () => {
     return (
         <div>
             <h1>volunteer</h1>
+            <div class="nav-img-text-container">
+            <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
+            <div class="nav-img-text-centered"><a class="nav-img-text-style" href="/donate">Yard Sign</a></div>
+            </div>
+            <div class="nav-img-text-container">
+            <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
+            <div class="nav-img-text-centered"><a class="nav-img-text-style" href="/donate">Make Phone Calls</a></div>
+            </div>
+            <div class="nav-img-text-container">
+            <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
+            <div class="nav-img-text-centered"><a class="nav-img-text-style" href="/donate">Knock Doors</a></div>
+            </div>
+            <div class="nav-img-text-container">
+            <a href="/donate"><img src="https://raw.githubusercontent.com/kevinhansberger/react-app-alpha/alpha/src/images/politician-app-btn-bg-01.jpg" class="nav-img-center" /></a>
+            <div class="nav-img-text-centered"><a class="nav-img-text-style" href="/donate">Write Letters</a></div>
+            </div>
             <BottomNavBar name='volunteer' />
         </div>
     )
